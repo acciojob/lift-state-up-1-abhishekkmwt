f682a6c5732a75960b442f08acf37fd38aa1b3f7
abhishekkmwt <kumawat.abhishek167@gmail.com>
2023-08-18 17:15:36 +0530

    submit3

--- src/components/Child.js
+++ src/components/Child.js
@@ -2,24 +2,17 @@ import React,{useState} from "react";
 
 function Child({state,func}) {  
 
-    function f(){
-        if(state===false){
-            func(true)
-        }
-        else{
-            func(false)
-        }
-    }
+    
 
     return (
         <div className="child">
             <h2>Child Component</h2>
-            <button onClick={f}>Show Modal</button>
+            <button onClick={()=>func(true)}>Show Modal</button>
             {
-               state===true ? (<div className="modal">
+               state  && (<div className="modal">
                                 <h3>Modal Component</h3>
                                 <p>This is modal content</p>
-                            </div>) : ""
+                            </div>) 
             }
 
             
